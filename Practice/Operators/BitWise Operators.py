b44a7d452be7f61980d7d6b901d57f115df8d38f
vk3203 <learning.vamsi3203@gmail.com>
2024-01-14 19:55:37 +0530

    11

--- Practice/Operators/BitWise Operators.py
+++ Practice/Operators/BitWise Operators.py
@@ -1,4 +1,5 @@
-# Bitwise Operators
+# Bitwise Operators: applicable only for int and bool type
+
 a=input('Enter a value:')
 b=input('Enter b value:')
 #type casting
@@ -6,6 +7,7 @@ a=int(a)
 b=int(b)
 print('binary value of a:',bin(a))
 print('binary value of a:',bin(b))
+
 bitwise_AND = a&b
 print('value of a&b:',bitwise_AND)
 print('Binary of a&b:',bin(bitwise_AND))
@@ -21,14 +23,20 @@ print('value of a^b:',bitwise_XOR)
 print('Binary of a^b:',bin(bitwise_OR))
 print()
 
+
 #Bitwise complement operator
-x=int(input('Enter any number:'))
+x=int(input('Enter any number to shift:'))
 print(f'Complement of {x}, i.e., ~{x} is:{~x}')
+
 #Bitwise Shift operator
 n=int(input('Enter no.of digits to shift,n:'))
+
+# Left shift
 LeftShift=x<<n
 print(f'left shift by {n} digits of {x} is :{LeftShift}')
 print('Binary Value of left shift:',bin(LeftShift))
+
+# right shift
 RightShift=x>>n
 print(f'Right shift by {n} digits of {x} is :{x>>n}')
 print('Binary Value of Right shift:',bin(RightShift))
