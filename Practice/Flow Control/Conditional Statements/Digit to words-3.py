12948649fc0b54d71cfc444dd6fb458fc87e7ba4
vk3203 <learning.vamsi3203@gmail.com>
2024-01-15 20:55:09 +0530

    12

--- Practice/Flow Control/Conditional Statements/Digit to words-3.py
+++ Practice/Flow Control/Conditional Statements/Digit to words-3.py
@@ -1,3 +1,5 @@
+# Covert Digit to Word 0-9999
+
 words_upto_ones = ['','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Eleven','Tweleve','Thirteen','Fourteen','Fifteen','Sixteen','Seventeen','Eighteen','Nineteen']
 
 words_for_tens = ['','','Twenty','Thirty','Fourty','Fifty','Sixty','Seventy','Eighty','Ninty']
@@ -5,8 +7,6 @@ words_for_tens = ['','','Twenty','Thirty','Fourty','Fifty','Sixty','Seventy','Ei
 words_for_hundreds = ['','One Hundred','Two Hundred','Three hundred','Four Hundred','Five Hundred','Six Hundred','Seven Hundred','Eight Hundred','Nine Hundred']
 
 words_for_Thousand = ['','Thousand','Two Thousand','Three Thousand','Four Thousand','Five Thousand','Six Thousand','Seven Thousand','Eight Thousand','Nine Thousand']
-
-words_for_TenThousand = ['','Ten Thousand','Twenty Thousand','Thirty Thousand','FourtyThousand','Fifty Thousand','Sixty Thousand','Seventy Thousand','Eighty Thousand','Ninty Thousand']
 
 n = int(input('Enter a digit from 0-999: '))
 output = " "
@@ -22,10 +22,7 @@ elif n <=999:
   
 elif n <=9999:
   output = words_for_Thousand[n//1000] +" "+ words_for_hundreds[(n//100)%10]+" And "+ words_for_tens[(n//10)%10]+" "+words_upto_ones[n%10]
-
-elif n <=99999:
-  output = words_for_Thousand[n//1000]+ 'Thousand' +" "+ words_for_hundreds[(n//100)%10]+" And "+ words_for_tens[(n//10)%10]+" "+words_upto_ones[n%10]
-
+  
 else:
   output = print('please enter a number from 0-99')
 print(output)
