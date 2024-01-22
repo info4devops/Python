a1b609896552c70f585ab85d2476a3265b90edc8
vk3203 <learning.vamsi3203@gmail.com>
2024-01-22 14:33:32 +0530

    12

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-print('--Version:1--')
-import Pack1.Module1
-Pack1.Module1.f1()
-Pack1.Module1.f2(10,20)
-
-
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+x=int(input('Enter x:'))
+if x<0:
+  print('false:Not a ')
+else:
+  str(x)==str(x)[::-1]
+  print('true')
+  
+  
