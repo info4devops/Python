4a805ece96d92703f9866d893151b6ca696975e3
vk3203 <learning.vamsi3203@gmail.com>
2024-01-22 12:06:46 +0530

    12

---
+++ Practice/Packages/Pack1/Module1.py
@@ -0,0 +1,10 @@
+# Module1
+def f1():
+  print('Hello This is Module1 present in pack1')
+
+# Function1
+def f2(a,b):
+  print('Hello This is from function2 in module1 present in pack1')
+  print(f'The Sum:{a+b}')
+  
+
---
+++ Practice/Packages/Pack1/Module2.py
@@ -0,0 +1,4 @@
+def f3(a,b):
+  print('Hello This is from function3 in module2 present in pack1')
+  print(f'The Product:{a*b}')
+
