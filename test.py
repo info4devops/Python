0cd4ad1de0f69a3eac075bfe002c59923ef35769
vk3203 <learning.vamsi3203@gmail.com>
2024-01-25 17:23:56 +0530

    12

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 class Test:
-    def __init__(selt):
-        print('constructor Execution')
-    def Test(self):
-        print('Method Execution')
-    
-t1=Test()
-Test()
-t1.Test()
+    # Declaring: Inside constructor using self
+    def __init__(self):
+        self.a=10 # instance variable
+        self.b=20
+    # Declaring: Inside Instance method using self
+    def m1(self):
+        self.c = 999 # instance variable
+t=Test()
+print(t.__dict__)
+t.m1() # Calling instance method 
+print(t.__dict__)
