1504f8a784144c2bcf5efaa55655a3a5c24bb9a4
vk3203 <learning.vamsi3203@gmail.com>
2024-01-31 17:22:29 +0530

    12

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 f=open('abc.txt')
-print(f'Name of the file:{f.name}')
-print(f'Mode:{f.mode}')
-print(f'Is Readable:{f.readable()}')
-print(f'Is Writable:{f.writable()}')
-print(f'Is Closed:{f.closed}')
-f.close()
-print(f'Is Closed:{f.closed}')
+f.read()
+
+
 
 
