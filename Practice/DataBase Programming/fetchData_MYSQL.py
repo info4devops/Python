34050f87efb798f277d1c92658b5eb7f9333be84
vk3203 <learning.vamsi3203@gmail.com>
2024-02-12 00:04:03 +0530

    12

---
+++ Practice/DataBase Programming/fetchData_MYSQL.py
@@ -0,0 +1,47 @@
+# Create table, insert data and fetching that data from mySQL
+
+
+import pymysql
+
+try:
+    con = pymysql.connect(host='localhost', database='vamsidb', user='root', password='root')
+
+    cursor = con.cursor()
+
+    # Create the table with correct field names and data types
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS cricketers(sno INT(5), sname VARCHAR(12), sfee DOUBLE(10,2), saddr VARCHAR(12))')
+
+    # Insert data into the table using parameterized query
+    query = "INSERT INTO cricketers(sno, sname, sfee, saddr) VALUES (%s, %s, %s, %s)"
+    records = [(100, 'rohit', 1000, 'Mumbai'),
+               (101, 'Virat', 2000, 'Blr'),
+               (102, 'Gill', 3000, 'Pune'),
+               (103, 'Surya', 4500, 'Hyd')]
+    cursor.executemany(query, records)
+
+    # Commit the transaction
+    con.commit()
+    print('Records Inserted Successfully')
+    
+    # Fetching data from the table
+    cursor.execute('select * from cricketers')
+    data=cursor.fetchall()
+    print('Data Present in cricketers table')
+    for row in data:
+      print('Number:',row[0])
+      print('Name:',row[1])
+      print('Salary:',row[2])
+      print('Address:',row[3])
+      print()
+
+except pymysql.DatabaseError as e:
+    if con:
+        con.rollback()
+        print('There is a problem with SQL:', e)
+
+finally:
+    if cursor:
+        cursor.close()
+    if con:
+        con.close()
